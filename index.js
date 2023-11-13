72dba3bbc82dd187bd78c12bf96bb34eff81f8b2
only1tarunno <tarunno281@gmail.com>
2023-11-13 20:47:16 +0600

    cart data show

--- index.js
+++ index.js
@@ -29,14 +29,29 @@ async function run() {
     const reviewCollection = client
       .db("bistroDB")
       .collection("reviewsCollection");
+    const cartCollection = client.db("bistroDB").collection("cartCollection");
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
 
+    // review collection api
+
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    // carts collection api
+
+    app.get("/carts", async (req, res) => {
+      const result = await cartCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     });
 
