c30e3ef9651b649fe19dc812ce78d8b4df6619f7
only1tarunno <tarunno281@gmail.com>
2023-11-15 19:26:55 +0600

    l

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    const userCollection = client.db("bistroDB").collection("usersCollection");
     const menuCollection = client.db("bistroDB").collection("menuCollection");
     const reviewCollection = client
       .db("bistroDB")
@@ -36,8 +37,20 @@ async function run() {
       res.send(result);
     });
 
-    // review collection api
+    // users related api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exixts" });
+      } else {
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      }
+    });
 
+    // review collection api
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
